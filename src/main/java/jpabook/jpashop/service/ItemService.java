3fd3618b6ac6a3586e55cd3a4dab00004cd71b22
ybchar <uiurihappy@sju.ac.kr>
2022-09-07 15:25:14 +0900

    feat: git test

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -26,7 +26,8 @@ public class ItemService {
     //플러쉬를 날린다는것은 영속성 컨텍스트에 있는 엔티티중에 변경된 것을 다 찾는다.
     //데이터를 관리할때 UpdateItemDTO를 파라미터로 이용할 수도 있음
     public void updateItem(Long itemId, String name, int price, int stockQuantity){
-        //find해서 item을 찾는다. //id를 기반으로 영속상태의 entity를 찾는다.
+        // find해서 item을 찾는다.
+        // id를 기반으로 영속상태의 entity를 찾는다.
         // id가 있으면 트랜잭션 안에서 엔티티를 조회해야 영속 상태 조회가 가능하고
         // 거기에 값을 변경해야 변경감지가 일어날 수 있다.
         Item findItem = itemRepository.findOne(itemId);
