cb4cec1499f18f70578de8e53804cd836e41dfe8
ybchar <uiurihappy@sju.ac.kr>
2022-11-06 18:15:10 +0900

    feat: member get V2

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // @Controller , @ResponseBody 합친 것
 @RestController
@@ -19,7 +21,42 @@ public class MemberApiController {
     // final class
     private final MemberService memberService;
 
-    // 회원 등록 API
+    // 기존 안 좋은 예시 v1
+    @GetMapping("/get")
+    public List<Member> memberV1(){
+        // 1. 엔티티가 직접 외부에 노출되서 문제
+        // 2. 엔티티가 변경되면 API 스펙이 변경되므로 DTO를 사용하는 것을 권장
+        return memberService.findMembers();
+    }
+
+    // 회원 조회 API v2
+    @GetMapping("/getV2")
+    public MemberResult<List<MemberDTO>> memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDTO> members = findMembers.stream()
+                .map(item -> new MemberDTO(item.getName()))
+                .collect(Collectors.toList());
+        // 회원 수
+        Integer count = members.size();
+        return new MemberResult<>(count, members);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberResult<T> {
+        private Integer count;
+        private T data;
+    }
+
+    // 회원 조회 DTO
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO {
+        private String name;
+    }
+
+
+    // 회원 등록 API v2
     @PostMapping("/set")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         // Vaild javax validation이 자동으로 이뤄진다.
